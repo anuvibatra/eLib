b3db098bf03f7bcf22feb6c466336488903d2d45
anuvibatra <anuvibatra@gmail.com>
2020-10-26 23:21:10 +0800

    V1.0

--- src/eLibApp.py
+++ src/eLibApp.py
@@ -66,7 +66,6 @@ def deleteStudent(mycursor):
 def deleteBook(mycursor):
     book_name=input('Enter Book Name: ')
     sql='DELETE FROM BOOKS WHERE BNAME="' + book_name.upper() + '"'
-    #print(sql)
     mycursor.execute(sql)
     mydb.commit()
 
@@ -93,10 +92,16 @@ def bookAvailability(mycursor, book_name):
         for y in iResult:
             issuedBookCount = y[0]
         if issuedBookCount < numBooks:
-            print("This book available in the library.")
+            print("This book is available in the library.")
             return True
         else:
-            print("Sorry, all copies of this book are presently issued to students.")
+            query = 'SELECT MIN(RETURN_DATE) FROM IBOOKS WHERE ID=' + str(bookID)
+            mycursor.execute(query)
+            r = mycursor.fetchall()
+            a = ''
+            for z in r:
+                a=str(z[0])
+            print("Sorry, all copies of this book are presently issued to students. Earliest available after", a)
             return False
 
 def issueBook(mycursor):
@@ -123,53 +128,105 @@ def issueBook(mycursor):
                 bookID = y[0]
 
             issueBookSql = 'INSERT INTO IBOOKS VALUES (' + str(bookID) + ',' + str(student_roll_no) + ', NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY))'
-            print(issueBookSql)
             mycursor.execute(issueBookSql)
             mydb.commit()
 
+def listIssuedBooks(mycursor):
+    print("{0:<5}".format('ID'), "{0:<20}".format('RNO'), "{0:<20}".format('ISSUED_DATE'), "{0:<20}".format('RETURN_DATE'))
+    issuedbookssql='SELECT * FROM IBOOKS'
+    mycursor.execute(issuedbookssql)
+    result=mycursor.fetchall()
+    for z in result:
+        print("{0:<5}".format(z[0]), "{0:<20}".format(z[1]), "{0:<20}".format(str(z[2])), "{0:<20}".format(str(z[3])))
+
+def returnBook(mycursor):
+    returnBookName=input('enter book name: ')
+    returnbookRNO=int(input('enter student roll no.: '))
+    returnbooksSql = 'SELECT ID FROM BOOKS WHERE BNAME="' + returnBookName.upper() + '"'
+    mycursor.execute(returnbooksSql)
+    returnbookResult = mycursor.fetchall()
+    for d in returnbookResult:
+        returnbookID = d[0]
+        returnsql='DELETE FROM IBOOKS WHERE ID=' + str(returnbookID) + ' AND RNO=' + str(returnbookRNO) + ''
+        mycursor.execute(returnsql)
+        mydb.commit()
+
+def bookPastDueDate(mycursor):
+    bookspastdue= 'SELECT * FROM IBOOKS WHERE RETURN_DATE <= DATE_ADD(now(), INTERVAL -1 DAY);'
+    mycursor.execute(bookspastdue)
+    dueresult=mycursor.fetchall()
+    print("{0:<5}".format('ID'), "{0:<20}".format('RNO'), "{0:<20}".format('ISSUED_DATE'), "{0:<20}".format('RETURN_DATE'))
+    for p in dueresult:
+        print("{0:<5}".format(p[0]), "{0:<20}".format(p[1]), "{0:<20}".format(str(p[2])), "{0:<20}".format(str(p[3])))
 
 
-print('1. List available books in in library')
-print('2. Add new book')
-print('3. Delete a book')
-print('4. List all issued books')
-print('5. Check book availability')
-print('6. Issue a book')
-print('7. Return a book')
-print('8. List books that are past due date')
-print('9. List students')
-print('10. Add student')
-print('11. Delete student')
+def print_menu():
+    print('0. Print menu options')
+    print('1. List available books in library')
+    print('2. Add new book')
+    print('3. Delete a book')
+    print('4. List all issued books')
+    print('5. Check book availability')
+    print('6. Issue a book')
+    print('7. Return a book')
+    print('8. List books that are past due date')
+    print('9. List students')
+    print('10.Add student')
+    print('11. Delete student')
+
+print_menu()
 
 while True:
     print()
     print()
 
-    request = int(input('Select option: '))
+    request = input('Select option: ')
+    try:
+        request = int(request)
 
-    if request == 1:
-        listAvailableBooks(mycursor)
+        if request == 0:
+            print_menu()
 
-    if request == 2:
-        addNewBook(mycursor)
+        elif request == 1:
+            listAvailableBooks(mycursor)
 
-    if request == 3:
-        deleteBook(mycursor)
+        elif request == 2:
+            addNewBook(mycursor)
 
-    if request == 5:
-        bookAvailability(mycursor, None)
+        elif request == 3:
+            deleteBook(mycursor)
 
-    if request == 6:
-        issueBook(mycursor)
+        elif request == 4:
+            listIssuedBooks(mycursor)
 
-    if request == 9:
-        listStudents(mycursor)
+        elif request == 5:
+            bookAvailability(mycursor, None)
 
-    if request == 10:
-        addStudent(mycursor)
+        elif request == 6:
+            issueBook(mycursor)
 
-    if request == 11:
-        deleteStudent(mycursor)
+        elif request == 7:
+            returnBook(mycursor)
+
+        elif request == 8:
+            bookPastDueDate(mycursor)
+
+        elif request == 9:
+            listStudents(mycursor)
+
+        elif request == 10:
+            addStudent(mycursor)
+
+        elif request == 11:
+            deleteStudent(mycursor)
+
+        else:
+            print('Invalid option, try again.')
+
+    except:
+        print('Invalid option, try again.')
+
+
 
 
 
